df3d0e067f6d488fe1cf7176119d9f799b7fea41
valik_senyk <valentyn.senyk@eleks.com>
2021-06-02 21:28:35 +0300

    Add port to config

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const initListeners = require('./listeners');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const PORT = process.env.PORT;
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -60,8 +61,8 @@ mongoose.connect(
       /**
        * Server init
        */
-      http.listen(3000, () => {
-        console.log('listening on *:3000');
+      http.listen(PORT, () => {
+        console.log(`listening on *:${PORT}`);
       });
 
     });
